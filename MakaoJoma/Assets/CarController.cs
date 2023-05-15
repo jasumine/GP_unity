32a688e44e243cb28aace7355d0a195c346d1260
kinggodjs <92582758+jasumine@users.noreply.github.com>
2023-05-15 21:30:28 +0900

    0515(떨어질때 속도, 바퀴 움직임, cinema brain)
    떨어질때 속도를 더 빠르게 해주기
    바퀴 움직임
    window->pakage를 이용해서 camera가 car를 따라가게

--- MakaoJoma/Assets/CarController.cs
+++ MakaoJoma/Assets/CarController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
@@ -16,14 +17,22 @@ public class CarController : MonoBehaviour
 
     public bool grounded;
 
-    public Transform groundRay;
+    public Transform groundRay,groundAngle;
     public LayerMask Ground;
     public float rayLength = .7f;
+
+    private float dragGround;
+    public float gravity = 10f;
+
+    public Transform leftWheel, rightWheel;
+    public float maxWheelTurn;
 
 
     void Start()
     {
         RB.transform.parent = null;
+
+        dragGround = RB.drag;
     }
 
     // Update is called once per frame
@@ -43,16 +52,13 @@ public class CarController : MonoBehaviour
 
         turnInput = Input.GetAxis("Horizontal");
 
-        if(Input.GetAxis("Vertical")!=0)
-        {
-            // 현재의 roatation값에서 y축의 값을 넣어주면 회전이 된다.
-            // update문에서 컴퓨터의 성능의 차이를 주지않을려면 time.deltaTime을 써준다.
-            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles
-                + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Mathf.Sign(speedInput) *
-                (RB.velocity.magnitude / 40), 0f));
-        }
 
-        transform.position = RB.position;
+        // 바퀴꺾기
+        leftWheel.localRotation = Quaternion.Euler(leftWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn)-180,
+             leftWheel.localRotation.eulerAngles.z);
+        rightWheel.localRotation = Quaternion.Euler(rightWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn),
+            rightWheel.localRotation.eulerAngles.z);
+
     }
 
     // 컴퓨터의 성능마다 차이를 두지 않기위해 fixedUpdate사용
@@ -63,19 +69,60 @@ public class CarController : MonoBehaviour
         grounded = false;
 
         RaycastHit hit;
+        Vector3 normaltarget = Vector3.zero;
 
-        // groundRay의 위치에서, 아래로, 맞춘다, ray의 길이만큼, ground를
+        // groundRay의 위치에서, 아래로, 맞춘다, ray의 길이만큼, ground layer를
         if(Physics.Raycast(groundRay.position, -transform.up, out hit, rayLength, Ground))
         {
             grounded = true;
+
+            normaltarget = hit.normal;
+        }
+
+        // groundAngle의 위치에서, 아래로, 맞춘다, ray의 길이만큼, ground layer를
+        if (Physics.Raycast(groundAngle.position, -transform.up, out hit, rayLength, Ground))
+        {
+            grounded = true;
+
+            normaltarget = (normaltarget + hit.normal) / 2f;
         }
         
-        RB.AddForce(transform.forward * speedInput * 500f);
+
+            // 땅에서  normal 회전
+            if (grounded)
+        {
+            transform.rotation = Quaternion.FromToRotation(transform.up, normaltarget) *
+                transform.rotation;
+        }
+
+        // 땅에서 감속
+        if(grounded)
+        {
+            RB.drag = dragGround;
+            RB.AddForce(transform.forward * speedInput * 500f);
+        }
+        else
+        {
+            RB.drag = .1f;
+            RB.AddForce(-Vector3.up * gravity * 50f);
+        }
+
         
         if(RB.velocity.magnitude>Maxspeed)
         {
             RB.velocity = RB.velocity.normalized * Maxspeed;
         }
-        Debug.Log(RB.velocity.magnitude);
+
+        transform.position = RB.position;
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            // 현재의 roatation값에서 y축의 값을 넣어주면 회전이 된다.
+            // update문에서 컴퓨터의 성능의 차이를 주지않을려면 time.deltaTime을 써준다.
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles
+                + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Mathf.Sign(speedInput) *
+                (RB.velocity.magnitude / 40), 0f));
+        }
+
     }
 }
